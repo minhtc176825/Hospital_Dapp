6dcebe357bcc73fd2ca7538c707fb568b31170f0
Tran Cong Minh <minh.tc176825@gmail.com>
2020-09-12 11:15:37 +0700

    Updates

---
+++ precache-manifest.eca5d7c2f9f68917e1d99a412f7820c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c7173e289ae91aa304c54d03204eb0",
+    "url": "/hospital_react/index.html"
+  },
+  {
+    "revision": "8a36e21a1a43f90240c1",
+    "url": "/hospital_react/static/css/main.64e66807.chunk.css"
+  },
+  {
+    "revision": "893f90b6a6b06426a3e6",
+    "url": "/hospital_react/static/js/2.fb65dcf3.chunk.js"
+  },
+  {
+    "revision": "49423731f4b42bb1acb1fefa8741c67d",
+    "url": "/hospital_react/static/js/2.fb65dcf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a36e21a1a43f90240c1",
+    "url": "/hospital_react/static/js/main.6d02c784.chunk.js"
+  },
+  {
+    "revision": "8258505f35f1a1b68118",
+    "url": "/hospital_react/static/js/runtime-main.581648d3.js"
+  }
+]);
